66fe7887719df7ec39ba72075830129b026a98ce
Eber Camargo <bodacamargo@gmail.com>
2020-03-03 10:38:55 -0300

    gallery select pic ok

--- src/App.vue
+++ src/App.vue
@@ -28,8 +28,16 @@
       <vue-gallery
         :pics="[
           {
-            label: '',
-            src: '',
+            title: 'Lançamentos por Play Arquitetura',
+            label: 'Louças',
+            src: '/img/Foto1_Mantis.jpg',
+            description: 'Nossa nova parceria criativa é a Play Arquitetura. A mineira, fundada porMarcelo Alvarenga é responsável pela criação da linha de monocomandosMantis. O nome Mantis refere-se ao louva-deus. &quot;Pensamos que o desenhosuper longilíneo da bica, com uma leve inclinação, faz lembrar a delicadeza eelegância desses insetos&quot;, diz o arquiteto sobre sua criação exclusiva para aDocol.Criação do mesmo escritório, o chuveiro Toró é um projeto que pretende levaro prazer de um bom banho para qualquer lugar. Autoportante, o Toró foiprojetado pra que nem mesmo a falta de um ponto de água nas proximidadesseja empecilho, uma vez que pode ser conectado a uma mangueira comum dejardim, com um simples clique.',
+          },
+          {
+            title: 'Mix&amp;Match na Cozinha',
+            label: 'Louças',
+            src: '/img/Foto3_Mix_Match1.jpg',
+            description: 'A coleção Mix&amp;Match, reconhecida com os prêmios Good Design 2018 e iFDesign Award 2019, chega agora à cozinha renovando sua proposta deliberdade de escolha e a perfeita combinação entre forma e função. Com amesma precisão nos detalhes, proporções e acabamento, os componentes dalinha - bicas, duchas e acionamentos para torneira ou misturador - podem sercombinados de acordo com seu projeto e instalados em diversas posições nabancada, garantindo a flexibilidade da composição.',
           }
         ]"
       />
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="container">
-    <div class="container__gallery">
+    <a class="container__gallery">
       <div
         v-for="(pic, index) in pics"
         :key="index"
-        class="container__pic"
+        class="container__pic transition"
+        :class="selectedPic === index ? 'colored' : null"
+        @click="() => handleClick(index)"
       >
-        <img :src="pic.src">
-        <p class="container__pic__title">
+        <img
+          :key="index"
+          :src="pic.src"
+        >
+        <!-- <div class="container__pic__overlay" /> -->
+        <p
+          :key="index"
+          class="container__pic__label"
+        >
           {{ pic.label }}
         </p>
+      </div>
+    </a>
+    <div
+      v-for="(pic, index) in pics"
+      :key="index"
+      class="container__text"
+    >
+      <div
+        v-if="selectedPic === index"
+        class="container__text__show"
+      >
+        <vue-title>
+          {{ pic.title }}
+        </vue-title>
+        <vue-description>
+          {{ pic.description }}
+        </vue-description>
       </div>
     </div>
   </div>
@@ -24,27 +50,89 @@ export default {
       default: null,
     },
   },
+  data() {
+    return {
+      title: this.pics[0].title,
+      description: this.pics[0].description,
+      selectedPic: 0,
+    }
+  },
+  methods: {
+    handleClick(index) {
+      this.title = this.pics[index].title
+      this.description = this.pics[index].description
+      this.selectedPic = index
+    },
+  },
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   @import '@styles';
   .container {
     display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     width: 100%;
+    background-color: transparent;
     &__gallery {
-
+      display: flex;
+      justify-content: center;
+      background-color: $grey;
+      width: 100%;
     }
     &__pic {
+      position: relative;
+      background-color: transparent;
       width: 256px;
       height: 270px;
-      &__title {
+      margin-right: 15px;
+      filter: saturate(0);
+      cursor: pointer;
+      &:hover {
+        filter: saturate(100%);
+      }
+      &:last-child {
+        margin-right: 0;
+      }
+      img {
+        width: 256px;
+        height: 270px;
+      }
+      &__label {
+        position: relative;
+        bottom: 70px;
+        justify-content: center;
         text-align: center;
-        font-family: 'Moli';
+        font-family: 'Muli';
+        font-size: 20px;
         font-weight: 700;
         letter-spacing: -0.5px;
-        color: $white;
+        color: $text-light;
+        margin: 0;
       }
     }
+    &__text {
+      display: flex;
+      justify-content: center;
+      text-align: center;
+      flex-direction: column;
+      background-color: $grey;
+      width: 100%;
+      &__show {
+        margin:0 auto;
+      }
+    }
+  }
+  .colored {
+    filter: saturate(100%) !important;
+  }
+  .transition {
+    -webkit-transition: all 0.3s ease;
+    -moz-transition: all 0.3s ease;
+    -ms-transition: all 0.3s ease;
+    -o-transition: all 0.3s ease;
+    transition: all 0.3s ease;
   }
 </style>
