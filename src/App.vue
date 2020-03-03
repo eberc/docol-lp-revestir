994de13caaffe2f88534b98f8c128c1319ce72d9
Eber Camargo <bodacamargo@gmail.com>
2020-03-03 13:56:34 -0300

    card curiosidades ok

--- src/App.vue
+++ src/App.vue
@@ -30,37 +30,37 @@
           {
             title: 'Lançamentos por Play Arquitetura',
             label: 'Chuveiros',
-            src: '/img/Foto1_Mantis.jpg',
+            src: '/images/Foto1.jpg',
             description: 'Nossa nova parceria criativa é a Play Arquitetura. A mineira, fundada porMarcelo Alvarenga é responsável pela criação da linha de monocomandosMantis. O nome Mantis refere-se ao louva-deus. &quot;Pensamos que o desenhosuper longilíneo da bica, com uma leve inclinação, faz lembrar a delicadeza eelegância desses insetos&quot;, diz o arquiteto sobre sua criação exclusiva para aDocol.Criação do mesmo escritório, o chuveiro Toró é um projeto que pretende levaro prazer de um bom banho para qualquer lugar. Autoportante, o Toró foiprojetado pra que nem mesmo a falta de um ponto de água nas proximidadesseja empecilho, uma vez que pode ser conectado a uma mangueira comum dejardim, com um simples clique.',
           },
           {
             title: 'Mix&amp;Match na Cozinha',
             label: 'Torneiras',
-            src: '/img/Foto3_Mix_Match1.jpg',
+            src: '/images/Foto3.jpg',
             description: 'A coleção Mix&amp;Match, reconhecida com os prêmios Good Design 2018 e iFDesign Award 2019, chega agora à cozinha renovando sua proposta deliberdade de escolha e a perfeita combinação entre forma e função. Com amesma precisão nos detalhes, proporções e acabamento, os componentes dalinha - bicas, duchas e acionamentos para torneira ou misturador - podem sercombinados de acordo com seu projeto e instalados em diversas posições nabancada, garantindo a flexibilidade da composição.',
           },
           {
             title: 'Louças para durar e surpreender',
             label: 'Louças',
-            src: '/img/Foto6_Loucas.jpg',
+            src: '/images/Foto6.jpg',
             description: 'Com o mesmo padrão de qualidade dos metais e a mesma confiança daGarantia Toda Vida passamos a oferecer ao mercado nossas linhas de louças.São cinco linhas de bacias sanitárias com o sistema Limpeza 360º, dedescargas eficientes e remoção total de resíduos. Além disso, os produtos têmoutras tecnologias que garantem conforto e economia de água, comoSoftClose no assento, enchimento rápido e silencioso nas versões com caixaacoplada e a tecla com duas opções de acionamento Salvágua. As linhas decubas possuem acabamento impecável e uniforme e combinam perfeitamentecom os metais Docol. Com esses lançamentos pretendemos levar cada vezmais soluções completas ao mercado.',
           },
           {
             title: 'Cuba Concreta',
             label: 'Cubas',
-            src: '/img/Foto7_Concreta.jpg',
+            src: '/images/Foto7.jpg',
             description: 'Criação de Angelo Bucci, do escritório SPBR, as cubas da linha Concreta sãofabricadas em material cimentício, de uso inédito para este fim. A resistência doproduto é seis vezes superior ao cimento tradicional e tem baixíssimaporosidade. &quot;As características do material são potencialidades que provocama imaginação arquitetônica para encontrar uma aplicação prática onde apareçaa sua utilidade. O convite da Docol para o desenho de peças moldadas parabanheiros — esse segmento chamado louças sanitárias — surgiu comooportunidade de se juntar as pontas: Docol e Ductal.&quot;, conta Angelo Buccisobre seu projeto.',
           },
           {
             title: 'Docol e Mekal Due',
             label: 'Due',
-            src: '/img/Foto8_Due.jpg',
+            src: '/images/Foto8.jpg',
             description: 'Selando a parceria entre Docol e Mekal, apresentamos ao mercado a linhaDue. Composta por cuba Mekal e monocomando Docol, esse produto pretendetransformar a bancada de trabalho das cozinhas em um espaço multiuso, ondevocê pode recolher o misturador para dentro da cuba expandindo suaspossibilidades no espaço.O monocomando telescópico Due foi projetado para proporcionar versatilidadeaos projetos de cozinhas: seu funcionamento telescópico é leve eextremamente funcional. Pode ser instalado nos três modelos de cubas(simples, dupla e com escorredor) e também em cubas esculpidas; seudesenho prioriza a usabilidade - a bica em posição de uso é giratória em 360graus e possui arejador articulado de grande alcance.',
           },
           {
             title: 'Blink Ozônio',
             label: 'Blink Ozônio',
-            src: '/img/Foto8_Due.jpg',
+            src: '/images/Foto8.jpg',
             description: 'Evolução da torneira Blink Hospitalar com a tecnologia DocolPresence,consolidada no mercado como referência em inovação, esta nova torneiradesenvolvida a partir de uma parceria de pesquisa junto a hospitais, médicos eprofissionais de saúde, passa a contar com um novo aliado na luta contra aproliferação de microrganismos no ambiente hospitalar: o ozônio, um gásnatural que, quando misturado à água, tem o poder de eliminar 99% dasbactérias. Ideal para lavatórios de hospitais esse produto elimina agentespatogênicos e garante muito mais higiene nesses ambientes.',
           }
         ]"
@@ -68,18 +68,25 @@
     </vue-section>
     <!-- <vue-slider
       :images="[
-        '/img/Foto1.jpg',
-        '/img/Foto2.jpg',
-        '/img/Foto3.jpg',
-        '/img/Foto4.jpg',
-        '/img/Foto5.jpg',
-        '/img/Foto6.jpg',
-        '/img/Foto7.jpg',
-        '/img/Foto8.jpg',
+        '/images/Foto1.jpg',
+        '/images/Foto2.jpg',
+        '/images/Foto3.jpg',
+        '/images/Foto4.jpg',
+        '/images/Foto5.jpg',
+        '/images/Foto6.jpg',
+        '/images/Foto7.jpg',
+        '/images/Foto8.jpg',
       ]"
     /> -->
     <vue-section>
-      <vue-card />
+      <vue-title>Curiosidades</vue-title>
+      <vue-card
+        :card="{
+          icon: '/icons/icon1.svg',
+          title: 'SOBRE O STAND',
+          description: 'Nosso stand foi desenvolvido pelo Atelier do croata Marko Brajovic. Ainspiração deste ano foi a Natureza Urbana e cada espaço aborda um dosbiomas brasileiros. Esse conceito nasceu nas reuniões de briefing, durante asdiscussões sobre a importância do design, o Atelier propôs fazermos umahomenagem ao melhor de todos os designers: a natureza.',
+        }"
+      />
     </vue-section>
   </div>
 </template>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="card">
     <div class="card__wrapper">
-      dsads
+      <div class="card__icon-container">
+        <img :src="card.icon">
+      </div>
+      <div class="card__text-container">
+        <p class="card__text-container__title">
+          {{ card.title }}
+        </p>
+        <p class="card__text-container__description">
+          {{ card.description }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -9,6 +19,12 @@
 <script>
 export default {
   name: 'VueCard',
+  props: {
+    card: {
+      type: Object,
+      default: null,
+    },
+  },
 }
 </script>
 
@@ -17,14 +33,42 @@ export default {
   .card {
     width: 345px;
     height: 425px;
-    background-color: rebeccapurple;
+    border: 3px solid $grey;
     &__wrapper {
       @extend %wrapper;
+      justify-content: flex-start;
+      position: relative;
+      padding: 0;
       width: 270px;
-      background-color: bisque;
-      margin-top: 50px;
-      margin-left: auto;
-      margin-right: auto;
+      height: 100%;
+      margin: 0 auto;
+    }
+    &__icon-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 130px;
+      border-bottom: 3px solid $grey;
+    }
+    &__text-container {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      &__title {
+        font-family: 'Muli';
+        font-size: 14px;
+        font-weight: 700;
+        margin: 22px 0 25px 0;
+      }
+      &__description {
+        font-family: 'Muli';
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 18px;
+        color: $text;
+        margin: 0;
+      }
     }
   }
 </style>
